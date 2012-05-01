208a5e51fae37834187f50a5ab20633a1df3e02e
Anthony <apimentel.ucla@gmail.com>
2012-04-30 19:29:44 -0700

    quick update

--- GridSpace.java
+++ GridSpace.java
@@ -3,22 +3,35 @@ import javax.swing.ImageIcon;
 
 public class GridSpace {
     private Location m_loc;
-    //private Pokemon m_pokeOnSpace
+    private Pokemon m_pokeOnSpace;
     private String m_type;
     
     //To indicate spaces outside the boundary of the map, type = "Null"
     public GridSpace( int x, int y, String type ) {
         m_loc = new Location( x, y );
+        m_pokeOnSpace = null;
         m_type = type;
         } //end constructor GridSpace
         
-    public void addPokemonTo( /*Pokemon poke, */int x, int y ) {
-        //m_pokeOnSpace
+    public void addPokemonTo( Pokemon poke, int x, int y ) {
+        m_pokeOnSpace = poke;
+        m_pokeOnSpace.getLocation().setX( x );
+        m_pokeOnSpace.getLocation().setY( y );
         } //end method addPokemonTo
         
     public String getType() {
         return m_type;
         } //end method getType
+        
+    
+    public Pokemon getPokemon() {
+        return m_pokeOnSpace;
+        } //end method getPokemon
+    
+    
+    public void select() {
+        
+        } //end method select
         
     public Image getImage() {
         return new ImageIcon( "./Pics/" + m_type + "Space.png" ).getImage();
---
+++ Pokemon.java
@@ -0,0 +1,11 @@
+public class Pokemon {
+    private Location m_location;
+    
+    public Pokemon() {
+        m_location = new Location();
+        } //end constructor Pokemon
+        
+    public Location getLocation() {
+        return m_location;
+        } //end method getLocation
+    } //end class Pokemon
